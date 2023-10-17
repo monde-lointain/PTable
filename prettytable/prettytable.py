731c983fe79f1c88e0b602045117a1ebdcdba1d3
monde-lointain <hunterw297@gmail.com>
2023-10-17 07:38:04 -0500

    Remove dashes from title generation

--- prettytable/prettytable.py
+++ prettytable/prettytable.py
@@ -1248,17 +1248,9 @@ class PrettyTable(object):
 
         lines = []
         lpad, rpad = self._get_padding_widths(options)
-        if options["border"]:
-            if options["vrules"] == ALL:
-                options["vrules"] = FRAME
-                lines.append(self._stringify_hrule(options))
-                options["vrules"] = ALL
-            elif options["vrules"] == FRAME:
-                lines.append(self._stringify_hrule(options))
         bits = []
         endpoint = options["vertical_char"] if options["vrules"] in (ALL, FRAME) else " "
         bits.append(endpoint)
-        title = " " * lpad + title + " " * rpad
         bits.append(self._justify(title, len(self._hrule) - 2, "c"))
         bits.append(endpoint)
         lines.append("".join(bits))
